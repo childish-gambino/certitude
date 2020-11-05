1cdc6d75658d264051a41e85e1fb79b5acc99133
vikrant.tyagi <vikrant.tyagi@grofers.com>
2020-11-05 15:00:24 +0530

    delta sync from production certitude

--- iaudit/admin.py
+++ iaudit/admin.py
@@ -5,4 +5,6 @@ from .models import ScanCSV
 
 class ScannedMachines(admin.ModelAdmin):
     list_display = [field.name for field in ScanCSV._meta.get_fields()]
+    list_filter = ['appname']
+    search_fields = ('appname', 'email', 'serialnumber' )
 admin.site.register(ScanCSV,ScannedMachines)
